d10df30d2ed5eb0c6701662f910ec0497a642a2f
aliraad79 <a.ahmadi.k.79@gmail.com>
2023-03-09 11:43:27 +0330

    refactore insurance max ceil

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-from consts import ONE_HEMAT, average_income_1400, number_of_bimey_shodegan_1400
+from consts import (ONE_HEMAT, average_income_1400,
+                    number_of_bimey_shodegan_1400)
 from enums import MaxWageUpgrade
 
 
@@ -48,44 +49,3 @@ def print_govern_total(GOVERN_PERCENTAGE):
 
 def get_percentage(num1, num2) -> float:
     return round(num1 / num2, 3) * 100
-
-
-def insurance_perimum_ceil(
-    ceil: MaxWageUpgrade, data: pd.DataFrame, govern_percentage: float
-):
-    total_people = data.loc["total_number"].sum()
-
-    if ceil == MaxWageUpgrade.One_x:
-        unsuported = data.drop("min_wage", axis=1)
-        data = data[["min_wage"]]
-        max_supported_salary = data["min_wage"]["avg_wage"]
-
-    elif ceil == MaxWageUpgrade.Two_x:
-        unsuported = data.drop(["min_wage", "1_2x"], axis=1)
-        data = data[["min_wage", "1_2x"]]
-        max_supported_salary = data["1_2x"]["avg_wage"]
-
-    elif ceil == MaxWageUpgrade.Three_x:
-        unsuported = data.drop(columns=["min_wage", "1_2x", "2_3x"])
-        data = data[["min_wage", "1_2x", "2_3x"]]
-        max_supported_salary = data["2_3x"]["avg_wage"]
-
-    max_govern_share_pay = rial_to_hunder_toman(
-        calc_gorn_percentage(max_supported_salary, govern_percentage)
-    )
-
-    effected_people = unsuported.loc["total_number"].sum()
-    print(
-        f"\n\nThe {ceil.name} ceil will effect {get_percentage(effected_people,total_people)}% of current people"
-    )
-    print(
-        f"For those people govern only pay {format_three_digit(max_govern_share_pay)} Rial per person"
-    )
-
-    unsuported.loc["Should pay on new ceil (hunder toman)"] = round(
-        unsuported.loc["original_govern_share"] - max_govern_share_pay
-    )
-    unsuported.loc["Should pay on new ceil (Percentage of wage)"] = get_percentage(
-        unsuported.loc["Should pay on new ceil (hunder toman)"], unsuported.loc["avg_wage"]
-    )
-    print(unsuported)
---
+++ insurance_perimum_ceil.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from tabulate import tabulate
+
+from enums import MaxWageUpgrade
+from helper import (calc_gorn_percentage, format_three_digit, get_percentage,
+                    rial_to_hunder_toman)
+
+
+class insurancePerimumCeil:
+    def __init__(
+        self, ceil: MaxWageUpgrade, data: pd.DataFrame, govern_percentage: float
+    ) -> None:
+        self.ceil = ceil
+        self.data = data
+        self.govern_percentage = govern_percentage
+
+        self.total_number_of_people = data.loc["total_number"].sum()
+
+    def main(self):
+
+        if self.ceil == MaxWageUpgrade.One_x:
+            unsuported = self.data.drop("min_wage", axis=1)
+            self.data = self.data[["min_wage"]]
+            max_supported_salary = self.data["min_wage"]["avg_wage"]
+
+        elif self.ceil == MaxWageUpgrade.Two_x:
+            unsuported = self.data.drop(["min_wage", "1_2x"], axis=1)
+            self.data = self.data[["min_wage", "1_2x"]]
+            max_supported_salary = self.data["1_2x"]["avg_wage"]
+
+        elif self.ceil == MaxWageUpgrade.Three_x:
+            unsuported = self.data.drop(columns=["min_wage", "1_2x", "2_3x"])
+            self.data = self.data[["min_wage", "1_2x", "2_3x"]]
+            max_supported_salary = self.data["2_3x"]["avg_wage"]
+
+        max_govern_share_pay = rial_to_hunder_toman(
+            calc_gorn_percentage(max_supported_salary, self.govern_percentage)
+        )
+
+        effected_people = unsuported.loc["total_number"].sum()
+        self.percentage_of_effected_people = get_percentage(
+            effected_people, self.total_number_of_people
+        )
+
+        unsuported.loc["Should pay on new ceil (hunder toman)"] = round(
+            unsuported.loc["original_govern_share (hunderd toman)"]
+            - max_govern_share_pay
+        )
+        unsuported.loc["Should pay on new ceil (Percentage of wage)"] = get_percentage(
+            unsuported.loc["Should pay on new ceil (hunder toman)"],
+            unsuported.loc["avg_wage"],
+        )
+
+        self.print_log(effected_people, max_govern_share_pay, unsuported)
+
+    def print_log(self, effected_people, max_govern_share_pay, unsuported_people):
+        print(
+            f"\n\nThe {self.ceil.name} ceil will effect {self.percentage_of_effected_people}% of current people"
+        )
+        print(
+            f"For those people govern only pay {format_three_digit(max_govern_share_pay)} Rial per person"
+        )
+
+        print(tabulate(unsuported_people, headers="keys", tablefmt="psql"))
